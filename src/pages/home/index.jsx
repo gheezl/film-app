4492c56289312b09c2356682950f3646907f141c
Jacob Rushlow <gheezl25@gmail.com>
2023-05-19 16:41:44 -0400

    Improved the styling a bit more

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     const { nowPlaying, popularFilms, topRatedFilms, upcomingFilms } = useContext(TmdbContext)
 
     return (
-        <Box sx={{ padding: "20px" }}>
+        <Box sx={{ padding: "20px", marginBottom: "20px" }}>
             <FilmSetDisplay headLine={"Films Now Playing"} films={nowPlaying} />
             <FilmSetDisplay headLine={"Popular Films"} films={popularFilms} />
             <FilmSetDisplay headLine={"Highest Rated Films"} films={topRatedFilms} />
