1af20e52c155ff6da49aea9f939ea591deb7bf80
Jacob Rushlow <gheezl25@gmail.com>
2023-06-08 09:46:22 -0400

    Modifying the horizontal scroll for mobile

--- src/components/film-set-display/index.jsx
+++ src/components/film-set-display/index.jsx
@@ -2,7 +2,7 @@ import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 
 import FilmCard from "../../components/film-card";
 
-// import HorizontalScroll from "react-scroll-horizontal";
+import HorizontalScroll from "react-scroll-horizontal";
 
 const FilmSetDisplay = ({ headLine, films }) => {
     const theme = useTheme();
@@ -21,52 +21,55 @@ const FilmSetDisplay = ({ headLine, films }) => {
             >
                 {headLine}
             </Typography>
-
-            <Box
-                sx={{
-                    display: "flex",
-                    overflowX: "scroll",
-                    height: "380px",
-                    borderLeft: isBelowMd ? "none" : `1px solid ${theme.palette.primary.second}`,
-                    borderRight: isBelowMd ? "none" : `1px solid ${theme.palette.primary.second}`,
-                    borderRadius: "20px",
-                    padding: "0px 10px 0px 10px"
-                }}
-            >
-                {
-                    films?.results
-                        ? (
-                            films?.results.map(film => (
-                                film.poster_path
-                                    ? <FilmCard film={film} />
-                                    : null
-                            ))
-                        )
-                        : null
-                }
-            </Box>
-
-            {/* <HorizontalScroll
-                style={{
-                    height: "380px",
-                    borderLeft: isBelowMd ? "none" : `1px solid ${theme.palette.primary.second}`,
-                    borderRight: isBelowMd ? "none" : `1px solid ${theme.palette.primary.second}`,
-                    borderRadius: "20px",
-                    padding: "0px 10px 0px 10px"
-                }}
-            >
-                {
-                    films?.results
-                        ? (
-                            films?.results.map(film => (
-                                film.poster_path
-                                    ? <FilmCard film={film} />
-                                    : null
-                            ))
-                        )
-                        : null
-                }
-            </HorizontalScroll> */}
+            {
+                isBelowMd
+                    ?
+                    <Box
+                        sx={{
+                            display: "flex",
+                            overflowX: "scroll",
+                            height: "380px",
+                            borderLeft: isBelowMd ? "none" : `1px solid ${theme.palette.primary.second}`,
+                            borderRight: isBelowMd ? "none" : `1px solid ${theme.palette.primary.second}`,
+                            borderRadius: "20px",
+                            padding: "0px 10px 0px 10px"
+                        }}
+                    >
+                        {
+                            films?.results
+                                ? (
+                                    films?.results.map(film => (
+                                        film.poster_path
+                                            ? <FilmCard film={film} />
+                                            : null
+                                    ))
+                                )
+                                : null
+                        }
+                    </Box>
+                    :
+                    <HorizontalScroll
+                        style={{
+                            height: "380px",
+                            borderLeft: isBelowMd ? "none" : `1px solid ${theme.palette.primary.second}`,
+                            borderRight: isBelowMd ? "none" : `1px solid ${theme.palette.primary.second}`,
+                            borderRadius: "20px",
+                            padding: "0px 10px 0px 10px"
+                        }}
+                    >
+                        {
+                            films?.results
+                                ? (
+                                    films?.results.map(film => (
+                                        film.poster_path
+                                            ? <FilmCard film={film} />
+                                            : null
+                                    ))
+                                )
+                                : null
+                        }
+                    </HorizontalScroll>
+            }
         </Box>
     )
 }
